10f963e1a67097ec07966d89fcc9890b77974195
Stefan Hillmich <stefan.hillmich@jku.at>
2023-02-28 15:41:45 +0100

    Create package on heap
    - This may fix the segfault on Windows

--- test/main.cpp
+++ test/main.cpp
@@ -1,12 +1,13 @@
-#include <iostream>
-
 #include "dd/MDDPackage.hpp"
 
+#include <memory>
+#include <vector>
+
 int main() {
-  auto mdd = dd::MDDPackage(3, {2, 2, 3});
+  auto mdd = std::make_unique<dd::MDDPackage>(3, std::vector<std::size_t >{2, 2, 3});
   // auto zeroState = mdd.makeZeroState(3);  // number of particles in the rack
-  auto oneState = mdd.makeBasisState(3, {1, 1, 2});
-  mdd.printVector(oneState);
+  auto oneState = mdd->makeBasisState(3, {1, 1, 2});
+  mdd->printVector(oneState);
   // auto id = mdd.makeIdent(3);
 
   // auto x = mdd.getVector(zeroState);
