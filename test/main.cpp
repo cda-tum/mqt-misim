b9eee23ad7d471b34736f09dbf2d62290b44f3f2
kmatoxxx <kevin.mato@tum.de>
2023-03-09 13:50:33 +0100

    Style files re-added
    Signed-off-by: kmatoxxx <kevin.mato@tum.de>

--- test/main.cpp
+++ test/main.cpp
@@ -4,13 +4,10 @@
 
 int main() {
   auto mdd = dd::MDDPackage(3, {2, 2, 3});
-  // auto zeroState = mdd.makeZeroState(3);  // number of particles in the rack
-  auto oneState = mdd.makeBasisState(3, {1, 1, 2});
-  mdd.printVector(oneState);
-  // auto id = mdd.makeIdent(3);
 
-  // auto x = mdd.getVector(zeroState);
-  // auto U3 = dd::U3mat(dd::PI_4, -dd::PI_2, dd::PI_2);
+    auto oneState = mdd.makeBasisState(3, {1, 1, 2});
+
+  mdd.printVector(oneState);
 
   return 0;
 }
