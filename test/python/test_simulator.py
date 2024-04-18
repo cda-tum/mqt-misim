649b60f99497eef2ef41276745bc3aa44d259a77
burgholzer <burgholzer@me.com>
2024-04-18 20:11:02 +0200

    ✅ add a dummy python test
    Signed-off-by: burgholzer <burgholzer@me.com>

---
+++ test/python/test_simulator.py
@@ -0,0 +1,10 @@
+"""Basic tests for the simulator."""
+
+from __future__ import annotations
+
+# from mqt.misim import state_vector_simulation
+
+
+def test_simulator():
+    """Test the simulator."""
+    # TODO: add a proper test here
