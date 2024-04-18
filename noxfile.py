2415b6f876528d6d5aa924f36f791abc7cac45af
burgholzer <burgholzer@me.com>
2024-04-18 19:57:41 +0200

    📝 very basic RtD setup
    Signed-off-by: burgholzer <burgholzer@me.com>

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import argparse
 import os
 import shutil
 import sys
@@ -80,3 +81,35 @@ def minimums(session: nox.Session) -> None:
         run_args=["-Wdefault"],
     )
     session.run("uv", "pip", "list")
+
+
+@nox.session(reuse_venv=True)
+def docs(session: nox.Session) -> None:
+    """Build the docs. Use "--non-interactive" to avoid serving. Pass "-b linkcheck" to check links."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", dest="builder", default="html", help="Build target (default: html)")
+    args, posargs = parser.parse_known_args(session.posargs)
+
+    serve = args.builder == "html" and session.interactive
+    extra_installs = ["sphinx-autobuild"] if serve else []
+    session.install(*BUILD_REQUIREMENTS, *extra_installs)
+    session.install("--no-build-isolation", "-ve.[docs]")
+    session.chdir("docs")
+
+    if args.builder == "linkcheck":
+        session.run("sphinx-build", "-b", "linkcheck", ".", "_build/linkcheck", *posargs)
+        return
+
+    shared_args = (
+        "-n",  # nitpicky mode
+        "-T",  # full tracebacks
+        f"-b={args.builder}",
+        ".",
+        f"_build/{args.builder}",
+        *posargs,
+    )
+
+    if serve:
+        session.run("sphinx-autobuild", "--ignore", "**jupyter**", *shared_args)
+    else:
+        session.run("sphinx-build", "--keep-going", *shared_args)
